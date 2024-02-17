74c3b6295542c49d246486fe6946c63af51131d3
Mannaja78520 <72680123+Mannaja78520@users.noreply.github.com>
2024-02-18 01:31:10 +0700

    Add files via upload

---
+++ zipLine/zipLine.ino
@@ -0,0 +1,27 @@
+#include "Robot.h"
+
+/** Usage External Class */
+Robot Robot;
+
+boolean carCheck(float distance){
+  return (Robot.UltraSonic() > distance); 
+}
+
+void drop(){
+  Robot.servo1.write(100);
+  delay(100);
+}
+
+void setup(){
+  Serial.begin(115200);
+  Robot.init();
+  while(carCheck(35)){
+    Robot.MovePower(255);
+  }
+  Robot.MovePower(0);
+  drop();
+}
+
+void loop(){
+
+}
